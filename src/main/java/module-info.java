872d922cd29072ba1f0071e2c57e10c4115faee2
Darshitha <darshtech06@gmail.com>
2025-01-17 11:10:07 +0530

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module darshita.project.grokking_dsa {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens darshita.project.grokking_dsa to javafx.fxml;
+    exports darshita.project.grokking_dsa;
+}
