d4a62e79a09af3d3f9690f5e0a68706a9ad48223
Darshitha <darshtech06@gmail.com>
2025-02-20 21:09:05 +0530

    pushed

--- src/main/java/darshita/project/grokking_dsa/ProductLessThanTarget_TwoPointers.java
+++ src/main/java/darshita/project/grokking_dsa/ProductLessThanTarget_TwoPointers.java
@@ -1,4 +1,33 @@
 package darshita.project.grokking_dsa;
+import java.util.*;
 
 public class ProductLessThanTarget_TwoPointers {
+    public static List<List<Integer>> integerList(int[] arr, int target) {
+        int n = arr.length, left = 0, right = n - 1, subprdct = 1;
+        List<List<Integer>> ans = new ArrayList<>();
+        while(left <= right) {
+
+            subprdct = arr[left]*arr[right];
+            if(subprdct == target) {
+                List<Integer> lst = new ArrayList<>();
+                lst.add(arr[left]);
+                lst.add(arr[right]);
+                ans.add(lst);
+                left++;
+                right--;
+            }
+            else if(subprdct < target) {
+                left++;
+            }
+            else{
+                right--;
+            }
+
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] arr = {2,3,5,10};
+        System.out.println(integerList(arr, 30));
+    }
 }
--- src/main/java/darshita/project/grokking_dsa/TripletClosestSum.java
+++ src/main/java/darshita/project/grokking_dsa/TripletClosestSum.java
@@ -1,5 +1,11 @@
 package darshita.project.grokking_dsa;
 
 public class TripletClosestSum {
+    public static void tripletClosestSum(int[] arr , int targetSum){
+        int smallestDiff = Integer.MAX_VALUE;
+        for(int i = 0 ; i < arr.length-2 ; i++){
+
+        }
+    }
 
 }
